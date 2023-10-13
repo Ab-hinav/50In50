157a7cbee83d48413a6683caed7537e7484dd5ee
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Air.local>
2023-10-13 09:24:01 +0530

    expanding cards using only 1 event listner

--- Expanding_Cards/scripts/index.js
+++ Expanding_Cards/scripts/index.js
@@ -1,19 +1,18 @@
 
 
-const ctr = document.querySelectorAll('.img');
-// console.log(ctr);
+const ctr = document.querySelector('.img');
+const mp = new Map();
+const container = document.querySelector('.container');
+mp.set('active',ctr);
 
-function removeActiveClass() {
-
-    for(const i of ctr){
-        i.classList.remove('active');
-    }
+function removeActiveClass(activeDiv) {
+    const currActive = mp.get('active');
+    currActive.classList.remove('active');
+    mp.set('active',activeDiv);    
 }
 
-const container = document.querySelector('.container');
-
 container.addEventListener('click',(e) => {
-    console.log(e.target.closest('.img'));
-    removeActiveClass();
-    e.target.closest('.img').classList.toggle('active');
+    console.log(e.target);
+    removeActiveClass(e.target);
+    e.target.classList.toggle('active');
 })
